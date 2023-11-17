19ef786c28666f56ab0ce2729fa3dddceb35045a
narangrg443 <narangrg48@gmail.com>
2023-11-17 12:07:28 +0545

    Polished console

--- public/custom_eruda.js
+++ public/custom_eruda.js
@@ -1,4 +1,5 @@
-
+(function(){
+  
 
 let consoleVisible = true; // Track whether the console box is visible
 
@@ -9,12 +10,18 @@ const toggleConsole = () => {
   messageContainer.style.flexDirection='column';
   messageContainer.style.borderRadius='10px';
 };
+
+const clearConsole=()=>{
+  messageContainer.innerHTML='<b>ConsoleNG-v1.0.0</b>';
+}
+
+
 // Function to append a message to the console box
 const appendMessage = (message, type) => {
   let color;
 
   if (type === 'CONSOLE_MSG') {
-    color = 'white';
+    color = '#06ffdb';
   } else if (type === 'CONSOLE_ERR') {
     color = 'red';
   }
@@ -31,15 +38,16 @@ const appendMessage = (message, type) => {
 
   // Create a span for the message type
   const messageTypeSpan = document.createElement('span');
+  
   messageTypeSpan.textContent = type === 'CONSOLE_MSG' ? 'LOG: ' : 'ERROR: ';
-  messageTypeSpan.style.color = color;
+// messageTypeSpan.style.color = color;
   parentDiv.appendChild(messageTypeSpan);
 
   // Create a span for the message content
   const messageContentSpan = document.createElement('span');
   messageContentSpan.textContent = message;
-  messageContentSpan.style.color = color;
-  parentDiv.appendChild(messageContentSpan);
+   messageContentSpan.style.color = color;
+   parentDiv.appendChild(messageContentSpan);
 
   messageContainer.appendChild(parentDiv);
     messageContainer.style.borderRadius='10px';
@@ -69,16 +77,37 @@ toggleButton.style.display='absolute';
 toggleButton.style.bottom='2px';
 toggleButton.style.width="20px";
 
+//clear button
+const clearBtn = document.createElement('button');
+clearBtn.style.position='absolute';
+clearBtn.textContent = 'C';
+clearBtn.style.padding = '5px';
+clearBtn.style.cursor = 'pointer';
+clearBtn.style.backgroundColor = 'gray';
+clearBtn.style.color = 'white';
+clearBtn.addEventListener('click', clearConsole);
+clearBtn.style.display='absolute';
+clearBtn.style.bottom='2px';
+clearBtn.style.left='30px'
+clearBtn.style.width="20px";
+
+document.body.appendChild(clearBtn);
+
 // Create the initial message container
 const messageContainer = document.createElement('div');
 messageContainer.style.position='absolute';
 messageContainer.style.minWidth = '90%';
 messageContainer.style.minHeight = '50%';
+
 messageContainer.style.overflowY = 'auto';
 // messageContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
 messageContainer.style.color = 'white';
 messageContainer.style.padding = '10px';
 messageContainer.id = 'message-container';
+
+
+
+
 
 // Create a div container for the console box and button
 const consoleContainer = document.createElement('div');
@@ -93,12 +122,19 @@ document.body.appendChild(consoleContainer);
 
 
 // Override console.log to append messages to the console box
+// const originalConsoleLog = console.log;
+// console.log = (...args) => {
+//   const logMessage = "'"+args.map(String).join(' ')+"'";
+//   appendMessage(logMessage, 'CONSOLE_MSG');
+//   originalConsoleLog.apply(console, args);
+// };
 const originalConsoleLog = console.log;
 console.log = (...args) => {
-  const logMessage = args.map(String).join(' ');
+  const logMessage = args.map(arg => typeof arg === 'string' ? `"${arg}"` : arg).join(' ');
   appendMessage(logMessage, 'CONSOLE_MSG');
   originalConsoleLog.apply(console, args);
 };
+
 
 
 
@@ -118,7 +154,7 @@ window.addEventListener('error', (e) => {
  <div id='consoleContainer'>
   <div id='messageContainer'>
    <div id='parentDiv'>
-    <span id='messageTypeSpan'></span>
+    <span id='messageTypeSpan'>LOG:</span>
     <span id='messageContentSpan'></span>
    <div>
   </div>
@@ -130,3 +166,5 @@ window.addEventListener('error', (e) => {
 
 
 */
+
+})()
--- public/script.js
+++ public/script.js
@@ -1,11 +1,2 @@
-console.log('hello world');
-const arry=[3,4,5,6,4,7];
-
-arry.forEach(e=>{
-  console.log(e)
-})
-console.log(typeof arry);console.log('wtsup')
-const string=arry.join(',');
-console.log(string)
-string.replace(3,1)
-console.log(string)
+console.log(2)
+console.log('3')
